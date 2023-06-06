08c0620bcb062a983b743974b4b6bcd0bdd56ebd
David Varga <davidvarga986@gmail.com>
2023-06-06 15:51:25 +0200

    Add more effective solution for trailing zeros in factorial

--- Algorithms/trailingZerosFactorial.js
+++ Algorithms/trailingZerosFactorial.js
@@ -11,5 +11,20 @@ function trailingZeros(n) {
   }
   return counter;
 }
+// console.log(trailingZeros(10));
 
-console.log(trailingZeros(10));
+function trailingZerosImproved(n) {
+  if (n < 5) return 0;
+  return Math.floor(n / 5) + trailingZerosImproved(Math.floor(n / 5));
+}
+
+function trailingZerosIterative(n) {
+  let res = 0;
+  for (let i = 5; i <= n; i *= 5) {
+    res += Math.floor(n / i);
+  }
+  return res;
+}
+
+console.log(trailingZerosImproved(101));
+console.log(trailingZerosIterative(101));
