a95e93eaaf9f8656d6d81774b12ad6a5649854b9
David Varga <davidvarga986@gmail.com>
2023-06-07 16:49:21 +0200

    Add naive solution for isPrime

---
+++ Algorithms/isPrime.js
@@ -0,0 +1,11 @@
+function isPrimeNaive(n) {
+  if (n == 1) return false;
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
