3333e506c7806f2ba91be1ed8907827ee04db3f4
DavidVarga <davidvarga986@gmail.com>
2023-06-11 18:19:47 +0200

    Add more efficient solution for all divisors

--- Algorithms/Numbers/allDivisors.js
+++ Algorithms/Numbers/allDivisors.js
@@ -7,4 +7,32 @@ function allDivisors(num) {
   console.log(num);
 }
 
-allDivisors(12);
+// allDivisors(12);
+
+// efficient, but not in order
+function allDivisors2(num) {
+  for (let i = 1; i * i <= num; i++) {
+    if (i * i === num) {
+      console.log(i);
+      break;
+    }
+    if (num % i === 0) {
+      console.log(i);
+      console.log(num / i);
+    }
+  }
+}
+// allDivisors2(25);
+
+// efficient in order
+function allDivisors3(num) {
+  let i;
+  for (i = 1; i * i < num; i++) {
+    if (num % i === 0) console.log(i);
+  }
+  for (i; i >= 1; i--) {
+    if (num % i === 0) console.log(num / i);
+  }
+}
+allDivisors3(25);
+allDivisors3(24);
