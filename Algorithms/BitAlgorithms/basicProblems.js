b74c92349e68996a6dca07f7b9a8a286db328d1f
David Varga <davidvarga986@gmail.com>
2023-06-18 01:57:45 +0200

    Add solution for xor from 1 to n

--- Algorithms/BitAlgorithms/basicProblems.js
+++ Algorithms/BitAlgorithms/basicProblems.js
@@ -34,3 +34,19 @@ function findLog(num) {
   }
   return counter;
 }
+
+// Given a number N, the task is to find the XOR of all
+// numbers from 1 to N.
+//   1 -> 1
+//  10 -> 11
+//  11 -> 00
+// 100 -> 100
+// 101 -> 001
+// 110 -> 111
+function xorOfAll(num) {
+  if (num % 4 === 0) return num;
+  if (num % 4 === 1) return 1;
+  if (num % 4 === 2) return num + 1;
+  if (num % 4 === 3) return 0;
+}
+console.log("xor 6:", xorOfAll(6));
